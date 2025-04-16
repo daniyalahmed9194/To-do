d12f1bf4357aa386cb306535142d91e7506c96ab
Abdur Rehman Daniyal <rehmanahm0987@gmail.com>
2025-04-17 04:18:21 +0500

    day 4 progress.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
 @Component({
   selector: 'app-root',
   imports: [HeaderComponent, UserComponent],
@@ -10,4 +11,8 @@ import { UserComponent } from "./user/user.component";
 })
 export class AppComponent {
   title = 'first-angular-app';
+  users = DUMMY_USERS;
+  onSelectUser(id: string) {
+    console.log('Selected user id:', id);
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , computed, signal, Input, Output, EventEmitter} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 import { CssSelector } from '@angular/compiler';
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -9,14 +9,30 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
+
+// usage of signals and computed properties
 export class UserComponent {
- selectedUser = DUMMY_USERS[randomIndex];
+ //selectedUser = signal(DUMMY_USERS[randomIndex]);
+//
+////  get imageUrl() {
+ // // return 'assets/users/'+ this.selectedUser().avatar
+////  }
+//
+//i//magePath = computed(() =>  'assets/users/' + this.selectedUser().avatar );
+//
+ //onClick() {
+ // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+ // this.selectedUser.set(DUMMY_USERS[randomIndex]);
+ //}
+ @Input({required: true}) avatar!: string;
+ @Input({required: true}) name!: string;
+ @Input({required: true}) id!: string;
+ @Output() select = new EventEmitter<string>();
+  get imageUrl() {
+    return 'assets/users/' + this.avatar;
+  }
 
- get imageUrl() {
-  return 'assets/users/'+ this.selectedUser.avatar
- }
-
- onClick() {
-  console.log('User clicked!');
- }
+  onSelected() {
+    this.select.emit(this.id);
+  }
 }
